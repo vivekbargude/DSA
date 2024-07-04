dc129f715644d9842c81a03891c00c735c91c9a8
Vivek Bargude <155740699+vivekbargude@users.noreply.github.com>
2024-07-04 14:27:33 +0530

    Create QueueInterFaceExercise1.java

---
+++ QueueInterFaceExercise1.java
@@ -0,0 +1,48 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+
+class Tester {
+    
+    public static Deque<Object> mergeQueue(Deque<Integer> intQueue, Deque<Character> charQueue) {
+	    Deque<Object> ans = new ArrayDeque<Object>();
+	    
+	    while(!intQueue.isEmpty()&&!charQueue.isEmpty()){
+	        ans.add(intQueue.poll());
+	        ans.add(charQueue.poll());
+	    }
+	    
+	    while(!intQueue.isEmpty()){
+	        ans.add(intQueue.poll());
+	        
+	    }
+	    while(!charQueue.isEmpty()){
+	        ans.add(charQueue.poll());
+	        
+	    }
+	    
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		
+		Deque<Integer> integerQueue = new ArrayDeque<Integer>();
+		integerQueue.add(3);
+		integerQueue.add(6);
+		integerQueue.add(9);
+		
+		Deque<Character> characterQueue = new ArrayDeque<Character>();
+        characterQueue.add('a');
+		characterQueue.add('e');
+		characterQueue.add('i');
+		characterQueue.add('o');
+		characterQueue.add('u');
+		characterQueue.add('b');
+		
+		Deque<Object> mergedQueue = mergeQueue(integerQueue, characterQueue);
+		
+		System.out.println("The elements in the merged queue are:");
+		for(Object element: mergedQueue)
+		    System.out.println(element);
+	    
+	}
+}
